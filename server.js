b17ac96c1152655c0f754b2174756de768dba990
Rahul karak <111314093+Rahulkarak@users.noreply.github.com>
2024-05-12 13:27:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoute.js'
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors'
 import path from 'path'
+import {fileURLToPath} from 'url'
 
 
 //config env
@@ -15,6 +16,10 @@ dotenv.config();
 
 //database config
 connectDB()
+
+//es module fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //test object
 const app = express()
